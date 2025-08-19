c99e725321047be7991b0551f7247a28fabf21f0
Bhaskor28 <bhaskorcse520@gmail.com>
2025-08-19 15:43:45 +0600

    doctor logined successully but dropdown not showing

--- Vexacare.Web/Controllers/DoctorsController.cs
+++ Vexacare.Web/Controllers/DoctorsController.cs
@@ -1,16 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vexacare.Application.Doctors.ViewModel;
+using Vexacare.Application.Patients.ViewModels;
+using Vexacare.Domain.Entities.PatientEntities;
+using Vexacare.Infrastructure.Data;
 
 namespace Vexacare.Web.Controllers
 {
     public class DoctorsController : Controller
     {
+        #region Fields
+        private readonly UserManager<Patient> _userManager;
+        private readonly SignInManager<Patient> _signInManager;
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        #endregion
+    
+        #region Constructor
+        public DoctorsController(
+        UserManager<Patient> userManager,
+        SignInManager<Patient> signInManager,
+        ApplicationDbContext context,
+        IWebHostEnvironment webHostEnvironment)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        #endregion
         public IActionResult Index()
         {
             return View();
         }
+        #region register
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(DoctorRegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new Patient
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    PhoneNumber = model.PhoneNumber,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    // Assign Patient role
+                    await _userManager.AddToRoleAsync(user, "Doctor");
+                    await _context.SaveChangesAsync();
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Doctors");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+        #endregion
+
+        
+
     }
 }
