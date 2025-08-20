cbad507cafca1800761bb15562803e4cc50b048d
Sazib204061 <mdsaziburrahman440@gmail.com>
2025-08-20 11:16:45 +0600

    add breifDetails field

--- Vexacare.Application/Products/ViewModels/ProductListVM.cs
+++ Vexacare.Application/Products/ViewModels/ProductListVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace Vexacare.Application.Products.ViewModels
         public int Id { get; set; }
         public string Name { get; set; }
         public ProductType ProductType { get; set; }
+        public string BreifDescription { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ProductImages { get; set; }
---
+++ Vexacare.Infrastructure/Migrations/20250820033716_Product BreifDescription field added in product table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vexacare.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductBreifDescriptionfieldaddedinproducttable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BreifDescription",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BreifDescription",
+                table: "Products");
+        }
+    }
+}
